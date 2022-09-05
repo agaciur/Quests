c23bf6699859df5fce87d1cec8cf86ef0e6f911b
Agata Rojewska <agata.rojewska@gmail.com>
2022-09-05 20:10:11 +0200

    zadanie z liczb pierwszych

--- src/Zadania/2022-09-04/1.php
+++ src/Zadania/2022-09-04/1.php
@@ -4,3 +4,17 @@ declare(strict_types=1);
 
 // Napisz program sprawdzający, czy dana liczba naturalna n jest pierwsza.
 
+function czyPierwsza(int $n)
+{
+    for ($i = $n - 1; $i >= 2; --$i) {
+        if ($n % $i === 0) {
+            return 'nie pierwsza';
+        }
+    }
+
+    return 'pierwsza';
+}
+
+echo czyPierwsza(18);
+
+
