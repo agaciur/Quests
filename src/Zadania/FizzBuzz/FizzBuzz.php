5cb848b604c96ca9034d6875982e04e10bc1402f
Jakub Maj <jakub@ittools.pl>
2022-09-10 14:40:06 +0200

    Add a new task Fizz Buzz

---
+++ src/Zadania/FizzBuzz/FizzBuzz.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zadania\Zadania;
+
+//Napisz program, który wyświetla liczby od 1 do 100.
+// Dla liczb podzielnych przez 3 niech program wyświetli „Fizz”;
+//dla liczb podzielnych przez 5 niech wyświetli 'Buzz’;
+//a dla liczb podzielnych przez 15 niech wyświetli 'FizzBuzz’.
+
+// Na potrzeby zadania nie zwracaj (return) danych z funkcji lecz wyświetl (echo)
+
+class FizzBuzz {
+    public function run() {
+        //TODO: IMPLEMENTATION
+    }
+}
+
+$fizz = new FizzBuzz();
+$fizz->run();
