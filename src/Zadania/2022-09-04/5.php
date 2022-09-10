7a32c80f32e3a888a0932e8cd344663ac7258416
Jakub Maj <jakub@ittools.pl>
2022-09-10 16:28:59 +0200

    Review for task 5

--- src/Zadania/2022-09-04/5.php
+++ src/Zadania/2022-09-04/5.php
@@ -8,41 +8,38 @@ declare(strict_types=1);
 
 class Kalkulator
 {
-    private int $suma;
-    private int $roznica;
-    private int $potega;
-    private int $pierwiastek;
-
-    public function suma (int $i, int $j): int
+    public function suma(int $i, int $j): int
     {
         return $i + $j;
     }
-    public function roznica (int $i, int $j): int
+
+    public function roznica(int $i, int $j): int
     {
         return $i - $j;
     }
-    public function potega (int $i, int $j): int
+
+    public function potega(int $i, int $j): int
     {
         return $i ** $j;
     }
-    public function pierwiastek (float $k): float
+
+    public function pierwiastek(float $k): float
     {
         return sqrt($k);
-
     }
 }
 
 $kalkulator = new Kalkulator();
 
 $suma = $kalkulator->suma(2, 5);
-echo 'Metoda sum() zwraca ' . ($suma === 7 ? 'poprawny':'niepoprawny') . ' wynik'.PHP_EOL;
+echo 'Metoda sum() zwraca ' . ($suma === 7 ? 'poprawny':'niepoprawny') . ' wynik' . PHP_EOL;
 
 $roznica = $kalkulator->roznica(2, 5);
-echo 'Metoda sub() zwraca ' . ($roznica === -3 ? 'poprawny':'niepoprawny') . ' wynik'.PHP_EOL;
+echo 'Metoda sub() zwraca ' . ($roznica === -3 ? 'poprawny':'niepoprawny') . ' wynik' . PHP_EOL;
 
 $potega = $kalkulator->potega(2, 5);
-echo 'Metoda sub() zwraca ' . ($potega === 32 ? 'poprawny':'niepoprawny') . ' wynik'.PHP_EOL;
+echo 'Metoda sub() zwraca ' . ($potega === 32 ? 'poprawny':'niepoprawny') . ' wynik' . PHP_EOL;
 
 $pierwiastek = $kalkulator->pierwiastek(16.0);
-echo 'Metoda sub() zwraca ' . ($pierwiastek === 4.0 ? 'poprawny':'niepoprawny') . ' wynik'.PHP_EOL;
+echo 'Metoda sub() zwraca ' . ($pierwiastek === 4.0 ? 'poprawny':'niepoprawny') . ' wynik' . PHP_EOL;
 
